a4a013250a4334090529b57c3ce77a79c634ab66
Octavio Noval <163550043+OctavioDNoval@users.noreply.github.com>
2025-09-18 23:51:36 -0300

    Add initial components for the portfolio: Contact, Footer, Header, Home, Projects, Skills, Index, and NotFound

---
+++ src/Components/FooterComponnet.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FooterComponnet = () => {
+	return <div>FooterComponnet</div>;
+};
---
+++ src/Components/ProjectsComponents.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ProjectsComponents = () => {
+	return <div>ProjectsComponents</div>;
+};
